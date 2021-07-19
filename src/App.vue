8a1ee6a5336a72145da13ca0ff5cfb0ea764eafe
nayinianJQ <1754630968abs@gmail.com>
2021-07-19 10:11:16 +0800

    更新

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <CesiumView />
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import CesiumView from './components/cesiums/views/index.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld,
+    CesiumView
   }
 }
 </script>
---
+++ src/components/cesiums/views/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="main"></div>    
+</template>
+<script>
+import cesium from 'cesium'
+
+export default {
+    mounted() {
+        var view = new cesium.Viewer("main", {
+            
+        })
+        window.console.log(view)
+    }
+}
+</script>
+<style lang="less" scoped>
+
+</style>
+
+
+
